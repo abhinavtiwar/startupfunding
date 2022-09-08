44092acd28aaf808fa4430cd1fabe84ea8a0aaca
Arpit Yadav <yadavarpit927@gmail.com>
2022-09-08 22:44:50 +0530

    ...

--- frontend/src/components/main/InvestorLogin.js
+++ frontend/src/components/main/InvestorLogin.js
@@ -1,44 +1,38 @@
-import './LoginUi.css';
+import "./LoginUi.css";
 import profile from "./image/a.png";
 import email from "./image/email.jpg";
 import pass from "./image/pass.png";
+import { Link } from "react-router-dom";
 function InvestorLogin() {
   return (
     <div className="main">
-     <div className="sub-main">
-       <div>
-         <div className="imgs">
-           <div className="container-image">
-             <img src={profile} alt="profile" className="profile"/>
-
-           </div>
-
-
-         </div>
-         <div>
-           <h1>Login Page</h1>
-           <div>
-             <img src={email} alt="email" className="email"/>
-             <input type="text" placeholder="user name" className="name"/>
-           </div>
-           <div className="second-input">
-             <img src={pass} alt="pass" className="email"/>
-             <input type="password" placeholder="user name" className="name"/>
-           </div>
-          <div className="login-button">
-          <button>Login</button>
+      <div className="sub-main">
+        <div>
+          <div className="imgs">
+            <div className="container-image">
+              <img src={profile} alt="profile" className="profile" />
+            </div>
           </div>
-           
-            <p className="link">
-              <a href="#">Forgot password ?</a> Or<a href="#">Sign Up</a>
-            </p>
-           
- 
-         </div>
-       </div>
-       
+          <div>
+            <h1>Login Page</h1>
+            <div>
+              <img src={email} alt="email" className="email" />
+              <input type="text" placeholder="user name" className="name" />
+            </div>
+            <div className="second-input">
+              <img src={pass} alt="pass" className="email " />
+              <input type="password" placeholder="password" className="name" />
+            </div>
+            <div className="login-button">
+              <button>Login</button>
+            </div>
 
-     </div>
+            <h3 className="text-center text-muted">
+              Don't have an account?<Link to="/main/InvestorSignup">Sign up</Link>
+            </h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
