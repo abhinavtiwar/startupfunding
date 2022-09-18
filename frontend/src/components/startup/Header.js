90e06a9dc2626efb3670181cf3db4150ca399658
Arpit Yadav <yadavarpit927@gmail.com>
2022-09-18 22:24:12 +0530

    .

--- frontend/src/components/startup/Header.js
+++ frontend/src/components/startup/Header.js
@@ -37,11 +37,6 @@ const Header = () => {
     </div>
   </nav>
  
-  <div class="p-5 text-center bg-light">
-    <h1 class="mb-3">Heading</h1>
-    <h4 class="mb-3">Subheading</h4>
-    <a class="btn btn-primary" href="" role="button">Call to action</a>
-  </div>
  
 </header>
     </div>
