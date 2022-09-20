a69ac786179c27d9693cb08f08d505db043cc820
sakshi28-08 <jsakshi058@gmail.com>
2022-09-20 13:23:29 +0530

    new

--- frontend/src/components/main/Listing.js
+++ frontend/src/components/main/Listing.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 
 function  Listing() {
+
+
+
   const [userArray, setUserArray] = useState([]);
   const [loading, setLoading] = useState(false);
 
@@ -37,55 +40,51 @@ function  Listing() {
       if (!loading) {
         return userArray.map(
           ({
-                title,
+                Startup  ,title,
                 email,
                 password,
                 year,
                 createdAt,
           }) =>(
             <div className="col-md-4 col-lg-4 mb-4 mb-lg-0">
-              <div className="card">
+              <div className="card mb-5">
                 <div className="d-flex justify-content-between p-3">
-                  <p className="lead mb-0">Today's Combo Offer</p>
+                  <p className="lead mb-0">Welcome To Our Project</p>
                   <div
                     className="bg-info rounded-circle d-flex align-items-center justify-content-center shadow-1-strong"
                     style={{ width: "35px", height: "35px" }}
                   >
-                    <p className="text-white mb-0 small">x4</p>
+                    <p className="text-white mb-0 small">Startup</p>
                   </div>
                 </div>
                 <img
-                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/4.webp"
+                  src="https://evolvers.co.in/wp-content/uploads/2019/11/Healthy-Startup.jpeg"
                   className="card-img-top"
-                  alt="Laptop"
+                  alt="Startup"
                 />
                 <div className="card-body">
                   <div className="d-flex justify-content-between">
                     <p className="small">
                       <a href="#!" className="text-muted">
-                        Laptops
+                       <h3> Startup</h3>
                       </a>
                     </p>
-                    <p className="small text-danger">
-                      <s>$1099</s>
+                    <p className="small">
+                      <h4>{year}</h4>
                     </p>
                   </div>
 
                   <div className="d-flex justify-content-between mb-3">
-                    <h5 className="mb-0">{title}</h5>
-                    <h5 className="text-dark mb-0">$999</h5>
+                    <h4 className="mb-0">{title}</h4>
+                    <h4 className="text-dark mb-0">{email}</h4>
                   </div>
 
                   <div className="d-flex justify-content-between mb-2">
                     <p className="text-muted mb-0">
-                      Available: <span className="fw-bold">6</span>
+                      <span className="fw-bold"></span>
                     </p>
                     <div className="ms-auto text-warning">
-                      <i className="fa fa-star"></i>
-                      <i className="fa fa-star"></i>
-                      <i className="fa fa-star"></i>
-                      <i className="fa fa-star"></i>
-                      <i className="fa fa-star"></i>
+                    <i class="fa-solid fa-thumbs-up"></i>
                     </div>
                   </div>
                 </div>
